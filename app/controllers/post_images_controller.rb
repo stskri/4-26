8325ee4a8f522c048c79551b16c56997f47e3ce7
stskri <177446189+stskri@users.noreply.github.com>
2024-08-28 12:42:14 +0000

    [finish]4-11

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -2,7 +2,7 @@ class PostImagesController < ApplicationController
   def new
     @post_image = PostImage.new
   end
-  
+
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
@@ -14,16 +14,17 @@ class PostImagesController < ApplicationController
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
   end
-  
+
   def index
     @post_images = PostImage.all
   end
-  
+
   private
 
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
-  
+
 end
